402b9c1e21e69d974d7f6926da843d1b1077c262
Nicola <nsprogramwork@mail.com>
2024-06-26 21:39:54 +0200

    init

---
+++ src/main/java/com/copy/copy_vesuviana/CopyVesuvianaApplication.java
@@ -0,0 +1,13 @@
+package com.copy.copy_vesuviana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyVesuvianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopyVesuvianaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/copy/copy_vesuviana/CopyVesuvianaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.copy.copy_vesuviana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopyVesuvianaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
